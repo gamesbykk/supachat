48db02ab475a3a4580f045230f491ebbbb239014
gamesbykk <127334503+gamesbykk@users.noreply.github.com>
2024-07-09 17:25:06 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import requests
+import json
+import streamlit as st
+
+prompt=st.chat_input()
+if prompt:
+  with st.chat_message('User'):
+    st.write(prompt)
+  out=requests.post(
+      "https://ishwarikatariya.app.modelbit.com/v1/chat/latest",
+      headers={"Content-Type": "application/json"},
+      data=json.dumps(
+          {
+              "data":f"System: Give response to prompt only. \n\n User: {prompt} \n\n Assistant:"
+          }
+      ),
+  ).json()
+  with st.chat_message('Assistant'):
+    st.write(out['data'])
